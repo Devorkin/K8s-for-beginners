6233f14bebb5d3cc104d31914ebcd4da539006ac
Yehonatan Devorkin <yehonatan@devorkin.net>
2023-01-26 10:55:38 +0200

    Update the Ingress-Nginx installation script to be wiser

--- Playground/Helm/Ingress-Nginx/ingress-nginx.sh
+++ Playground/Helm/Ingress-Nginx/ingress-nginx.sh
@@ -2,10 +2,24 @@
 
 if ! which helm &> /dev/null; then echo "Helm does not exists, please install it and re-run $0"; exit 1; fi
 
+# Script variables
+CPWD=$(basename $(pwd))
+if [ -f values.yaml ]; then
+  playground_dir="."
+elif [[ $CPWD == "K8s-for-begginers" ]]; then
+  playground_dir="./Playground"
+else
+  read -p 'Enter the path to the Playground directory: ' playground_dir
+  if [ ! -d $playground_dir ]; then
+    echo "Could not validate $playground_dir path"
+    exit 2
+  fi
+fi
+
 helm upgrade --install ingress-nginx ingress-nginx \
   --repo https://kubernetes.github.io/ingress-nginx \
   --namespace ingress-nginx --create-namespace \
-  -f values.yaml
+  -f $playground_dir/values.yaml
 
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
