25f482168d06019d02a5e8d3143cf5bdacb54be0
Yehonatan Devorkin <yehonatan@devorkin.net>
2023-03-09 13:35:13 +0200

    Add Kernel parameter change, vm.max_map_count=262144

--- provision_scripts/master.sh
+++ provision_scripts/master.sh
@@ -73,6 +73,7 @@ cat >> /etc/sysctl.d/kubernetes.conf << EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
+vm.max_map_count=262144
 EOF
   sysctl --system 2> /dev/null
 fi
